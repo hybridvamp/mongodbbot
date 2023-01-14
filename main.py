7f3b253654855aa21fc12a5379c174be9756edd9
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-01-14 13:03:13 +0400

    btns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 from io import BytesIO
 from telethon import TelegramClient, events
-from telethon.tl.types import KeyboardButton
+from telethon.tl.types import KeyboardButton, ReplyKeyboardMarkup
 from pymongo import MongoClient, errors
 from tqdm import tqdm
 
@@ -22,10 +22,10 @@ async def handler(message):
             # check the server status
             mongo_client.server_info()
             # send message with buttons
-            buttons = [
-                [events.Button.inline("Backup", data='backup'), events.Button.inline("Restore", data='restore')]
-            ]
-            await message.reply("Connection successful! Please select an option:", buttons=buttons)
+            button1 = KeyboardButton("Backup")
+            button2 = KeyboardButton("Restore")
+            inline_keyboard = [[button1, button2]]
+            await message.reply("Connection successful! Please select an option:", reply_markup=ReplyKeyboardMarkup(inline_keyboard))
         except errors.ServerSelectionTimeoutError as e:
             await message.reply(f"Connection failed: {e}")
         except Exception as e:
